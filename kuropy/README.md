c256c57f382367120340f5df99a861d0d6fa347c
Kuro <hello@kurojifusky.com>
2025-05-18 19:56:49 +0800

    add setuptools in kuropy

---
+++ kuropy/README.md
@@ -0,0 +1,3 @@
+# `kuropy`
+
+The Python package for cuties
---
+++ kuropy/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="kuropy",
+    version="0.1.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "bs4",
+        "requests"
+    ]
+)
